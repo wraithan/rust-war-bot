fa865993041d0434cee6a502f6381103b5cc71ea
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-06-01 10:05:20 -0700

    load files

--- tests/runner.rs
+++ tests/runner.rs
@@ -1,5 +1,22 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 include!(concat!(env!("OUT_DIR"), "/tests.rs"));
 
 fn run_file (name: &str) {
-    
+    let mut file_path = Path::new("tests/fodder").join(name);
+    file_path.set_extension("txt");
+
+    let mut file = match File::open(&file_path) {
+        Err(e) => {
+            println!("Couldn't open {}: {}", file_path.display(), e);
+            panic!(e);
+        },
+        Ok(f) => f
+    };
+
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    println!("{}", s);
 }
